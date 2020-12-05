0c588328895148354920affbb05d8ef4f693637b
Yuya Kono <takohati0821@gmail.com>
2020-12-05 22:52:16 +0900

    new impementation

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-ruby '2.6.2'
+ruby '2.7.2'
 
 source "https://rubygems.org"
 
@@ -8,3 +8,5 @@ gem 'discordrb'
 gem 'solargraph', group: :development
 gem 'pry', group: :development
 gem 'pry-doc', group: :development
+
+gem "bcdice", :git => "https://github.com/bcdice/BCDice.git", :ref => "f1973c7110657e88c1d14e949eab590ad9851b47"
--- trpgbot.rb
+++ trpgbot.rb
@@ -1,128 +1,70 @@
 STDOUT.sync = true
 
+# Discord API
 require 'discordrb'
 
-$LOAD_PATH.unshift File.join(__dir__, 'BCDice', 'src')
-require_relative './BCDice/src/bcdiceCore'
-require_relative './BCDice/src/diceBot/DiceBot'
-require_relative './BCDice/src/diceBot/DiceBotLoader'
-
-class BCDice
-  DICEBOTS = (DiceBotLoader.collectDiceBots + [DiceBot.new]).map do |dice_bot|
-    [dice_bot.gameType, dice_bot]
-  end.sort.to_h.freeze
-
-  SYSTEMS = DICEBOTS.keys.sort.freeze
-
-  NAMES = DICEBOTS.map { |game_type, dice_bot| { system: game_type, name: dice_bot.gameName } }
-                  .freeze
-end
+# BCDice
+require 'bcdice'
 
 module TRPGBot
-  class UnsupportedDicebot < StandardError; end
-
-  class CommandError < StandardError; end
-
-  class ToStringClient
-    attr_accessor :output
-    def quit; end
-
-    def sendMessage(_to, message)
-      @output ||= ""
-      @output << message
-      @output << "\r\n"
-    end
-  
-    def sendMessageToOnlySender(_nick_e, message)
-      @output ||= ""
-      @output << message
-      @output << "\r\n"
-    end
-  
-    def sendMessageToChannels(message)
-      @output ||= ""
-      @output << message
-      @output << "\r\n"
-    end
-
-    def getMessage
-      o = @output
-      @output = ""
-      o
-    end
-  end
-
-  # TRPG bot on Discord
+  # TRPG bot definition
   class Bot < Discordrb::Commands::CommandBot
-    # @return [Hash] bot config for each channels
-    attr_accessor :env_table
-    # @return [BCDice]
-    attr_reader :bcdice
+    include BCDice
 
     def initialize(*args)
       super(*args)
-      @bcdice = BCDiceMaker.new.newBcDice
-      @toStr = ToStringClient.new
-      @bcdice.setIrcClient(@toStr)
-      @env_table = {}
+      # TODO: serverごとに異なるダイスボットを利用できるようにする
+      @game_system = GameSystem::DiceBot
     end
 
-    # @param id [Integer] channel id
-    # @return [Hash] bot config for the channel
-    def env(id)
-      @env_table ||= {}
-      @env_table[id] ||= { system: 'DiceBot' }
-      @env_table[id]
+    def update_nickname(event)
+      self.member(event.channel.server, self.profile.id).nickname = "TRPGBot (#{@game_system::NAME})"
+      nil
     end
 
-    # @param id [Integer] channel id
-    # @param key [Symbol] key of config
-    # @param val [Object] new value
-    def set_env(id, key, val)
-      @env_table ||= {}
-      @env_table[id] ||= { system: 'DiceBot' }
-      @env_table[id][key] = val
+    # ゲームシステムを切り替える
+    def load_game_system(event, game_system)
+      klass = BCDice.dynamic_load(game_system)
+      if klass
+        @game_system = klass
+        event << "#{@game_system::NAME} loaded"
+      else
+        event << "#{game_system} not found"
+      end
     end
 
-    def update_bcdice(system, command)
-      dicebot = BCDice::DICEBOTS[system]
-
-      raise UnsupportedDicebot if dicebot.nil?
-
-      raise CommandError if command.nil? || command.empty?
-
-      @bcdice.setDiceBot(dicebot)
-      @bcdice.setMessage(command)
-      @bcdice.setDir('bcdice/extratables', system)
-      @bcdice.setCollectRandResult(true)
-      @bcdice
+    def show_help(event)
+      help_message = <<~HELP
+        TrpgBot commands:
+          use GAME_SYSTEM   # 使うゲームシステムを指定する
+          set GAME_SYSTEM   # 同上
+          help              # このメッセージを表示する
+      HELP
+      event << help_message
+      event << <<~HELP
+        ・ 加算ロール　　　　　　（xDn）（n面体ダイスをx個）
+        ・ バラバラロール　　　　（xBn）
+        ・ 個数振り足しロール   （xRn[振り足し値]）
+        ・ 上方無限ロール      （xUn[境界値]）
+        ・ シークレットロール   （Sダイスコマンド, 現在未実装）
+        ・ 四則演算（端数切捨て）（C(式)）
+      HELP
+      event << @game_system::HELP_MESSAGE
     end
 
-    def help(id)
-      system = env(id)[:system]
-      dicebot = BCDice::DICEBOTS[system]
-      output = <<"EOS"
-- 加算ロール　　　　　　（xDn）（n面体ダイスをx個）
-- バラバラロール　　　　（xBn）
-- 個数振り足しロール   （xRn[振り足し値]）
-- 上方無限ロール      （xUn[境界値]）
-- シークレットロール   （Sダイスコマンド）（DMで結果が届く）
-- 四則演算（端数切捨て）（C(式)）
-EOS
-      output << dicebot.getHelpMessage
-      output
+    def parse_command(message)
+      /^#{@game_system.command_pattern}$/.match message
+      $& #return matched string
     end
 
-    def diceroll(id, command)
-      system = env(id)[:system]
-      bcdice = update_bcdice(system, command)
-      result, secret = bcdice.dice_command
-
-      puts "DICEROLL: #{[system, command]} #{[result, secret]}"
-
-      raise CommandError if result.nil? || result == '1'
-
-      [result, secret]
+    def run_command(command)
+      gs = @game_system.new(command)
+      result = gs.eval().text
+      puts "DEBUG: #{result}"
+      result
+    rescue StandardError => e
+      puts e
+      puts e.backtrace
     end
   end
 end
@@ -133,39 +75,26 @@ bot = TRPGBot::Bot.new(
   ignore_bots: true
 )
 
-bot.command [:roll, :r, :dice] do |event, dice, system = nil|
-  save = bot.env_table
-
-  begin
-    bot.set_env(event.channel.id, :system, system) unless system.nil?
-
-    result, secret = bot.diceroll(event.channel.id, dice)
-    msg = BCDice::DICEBOTS[bot.env(event.channel.id)[:system]].gameName + result
-
-    bot.env_table = save
-    if secret
-      event.user.pm msg
-    else
-      event << msg
-    end
-  rescue TRPGBot::CommandError => e
-    event << e.to_s
-  end
-
-  nil
+bot.command :newSystemTest do |event|
+  event.respond 'OK!'
 end
 
-bot.command :set_system do |event, system|
-  bot.set_env(event.channel.id, :system, system)
-  "set system #{system} (#{BCDice::DICEBOTS[system].gameName})"
+bot.command [:use, :set] do |event, game_name|
+  bot.load_game_system event, game_name
+  bot.update_nickname event
 end
 
 bot.command :help do |event|
-  bot.help(event.channel.id)
+  bot.update_nickname event
+  bot.show_help event
 end
 
-bot.command :show_env do |event|
-  bot.env(event.channel.id).to_s
+bot.message do |event|
+  command = bot.parse_command event.content
+  if command
+    bot.update_nickname event
+    event << (bot.run_command command)
+  end
 end
 
 bot.run
